e9a4f5bdc49011949d7f9129e59def00b59f368f
RegisYamamoto <regisyamamoto@gmail.com>
2019-06-08 13:30:53 -0300

    inserindo uma nova Categoria com POST

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -35,7 +35,6 @@ public class Produto implements Serializable {
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
-	
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	@OneToMany(mappedBy="id.produto")
