da2deffebfa6004dced0fdc4f91b37a0e31ba113
RegisYamamoto <regisyamamoto@gmail.com>
2019-07-06 21:01:01 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -15,11 +15,11 @@ public class ItemPedido implements Serializable {
 	@EmbeddedId
 	@JsonIgnore
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
+
 	public ItemPedido() {
 	}
 
@@ -31,40 +31,56 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubtotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
+
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
+
 	public Double getDesconto() {
 		return desconto;
 	}
+
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
+
 	public Integer getQuantidade() {
 		return quantidade;
 	}
+
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
+
 	public Double getPreco() {
 		return preco;
 	}
+
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
@@ -93,5 +109,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
